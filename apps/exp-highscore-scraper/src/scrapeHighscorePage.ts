b0097691fe372a4fa7d8e1bd9ee29b0880521593
Rafal <rafaldoranczyk@gmail.com>
2026-03-01 19:07:38 +0100

    fix: add headers

--- apps/exp-highscore-scraper/src/scrapeHighscorePage.ts
+++ apps/exp-highscore-scraper/src/scrapeHighscorePage.ts
@@ -16,7 +16,13 @@ export async function scrapeHighscorePage({ world, vocationId, page }: Props) {
     const { data } = await axios.get(url, {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Referer: "https://www.tibia.com/community/?subtopic=highscores",
       },
     });
 
