aef1f4f5cc24faead194ce3087b1198b2aa6af49
Rafal <rafaldoranczyk@gmail.com>
2026-03-01 18:36:53 +0100

    fix: env variable

--- apps/exp-highscore-scraper/src/db/client.ts
+++ apps/exp-highscore-scraper/src/db/client.ts
@@ -1,12 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types/db.types";
 
-const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!(supabaseUrl && supabaseKey)) {
   throw new Error(
-    "Missing required environment variables: SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY"
+    "Missing required environment variables: NEXT_PUBLIC_SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY"
   );
 }
 
