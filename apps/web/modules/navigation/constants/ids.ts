3b71c90ac854c49e8dd6017814a9e5a6563748f0
Rafal <rafaldoranczyk@gmail.com>
2026-03-11 15:18:45 +0100

    refactor: images

---
+++ apps/web/modules/navigation/constants/ids.ts
@@ -0,0 +1,21 @@
+export const NAVIGATION_IDS = {
+  HOMEPAGE: "homepage",
+  // Character Overview
+  DASHBOARD: "dashboard",
+  CHARMS: "charms",
+  BESTIARY: "bestiary",
+
+  // Hunting
+  HUNT_SESSIONS: "hunt-sessions",
+  ADD_HUNT_SESSION: "add-hunt-session",
+  HUNT_ANALYTICS: "hunt-analytics",
+
+  // Utils
+  IMBUING: "imbuing-calculator",
+  TRAINING_CALCULATOR: "training-calculator",
+  STAMINA_CALCULATOR: "stamina-calculator",
+
+  // My Account
+  CHARACTERS: "characters",
+  LOGOUT: "logout",
+} as const;
